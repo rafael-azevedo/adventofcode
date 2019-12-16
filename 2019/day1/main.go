b0e38db8c87eed5ed4b0e2343f3cff112de15128
Rafael Azevedo <mail@rafaelazevedo.me>
2019-12-16 16:06:22 -0500

    Added day 1 part 2

--- 2019/day1/main.go
+++ 2019/day1/main.go
@@ -15,9 +15,7 @@ func main() {
 
 	fmt.Printf("Part 1 :: %d \n", CalcFuel(masses...))
 
-
-
-	fmt.Printf("Part 2 :: %d \n", CalcFuel(masses...))
+	fmt.Printf("Part 2 :: %d \n", calcFuelAlso(masses...))
 }
 
 
@@ -29,3 +27,23 @@ func CalcFuel(masses... int) int {
 	return totalFuel
 }
 
+func calcFuelAlso(masses... int) int {
+	totalFuel := 0
+	for _, m := range masses{
+		fuel :=  (m / 3) - 2
+		for fuel > 0 {
+			totalFuel = totalFuel + fuel
+			fuel = (fuel/3) -2
+		}
+	}
+	return int(totalFuel)
+}
+
+//func calcFuelRecursive(mass , fuel int64) (int64, int64) {
+//	if mass == 0 {
+//		return mass, fuel
+//	}
+//	diff := (mass/3) - 2
+//	fuel += diff
+//	return calcFuelRecursive(diff, fuel)
+//}
